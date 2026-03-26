83f406c72b33802df480053bfa5b74481c3d2a16
Sebastian Kember <81592324+DarkEnergy34909@users.noreply.github.com>
2026-03-26 19:11:38 +0000

    add imports

---
+++ app/routes/api/account.py
@@ -0,0 +1,105 @@
+from flask import make_response
+from email_validator import validate_email, EmailNotValidError
+
+from ...utils.auth import decode_jwt_token
+from ...utils.streak import format_streak
+from ...db.users import delete_account, email_exists_in_database, update_email_address, reset_streak_if_expired
+
+@app.route("/api/logout", methods=["POST"])
+def logout_api():
+    # Get the token from the request
+    token = request.cookies["token"]
+
+    # Decode the token
+    decoded_token = decode_jwt_token(token)
+
+    if ("error" in decoded_token):
+        return {"error": "invalid_token"}, 400
+
+    # Reset the HttpOnly cookie
+    response = make_response({"success": True}, 200)
+    response.set_cookie("token", "", domain="192.168.1.162", expires=0, httponly=True, secure=False, samesite='Lax')
+    return response
+
+@app.route("/api/account/delete", methods=["POST"])
+def delete_account_api():
+    # Get the token from the request
+    token = request.cookies["token"]
+
+    # Decode the token
+    decoded_token = decode_jwt_token(token)
+
+    if ("error" in decoded_token):
+        return {"error": "invalid_token"}, 400
+    
+    # Get the user id from the token
+    user_id = decoded_token["user-id"]
+
+    # Delete the user and all associated data from the database
+    delete_account(user_id)
+
+    # Reset the HttpOnly cookie
+    response = make_response({"success": True}, 200)
+    response.set_cookie("token", "", domain="192.168.1.162", expires=0, httponly=True, secure=False, samesite='Lax')
+    return response
+
+@app.route("/api/account/change-email", methods=["POST"])
+def change_email_api():
+    # Get the token from the request
+    token = request.cookies["token"]
+
+    # Decode the token
+    decoded_token = decode_jwt_token(token)
+
+    if ("error" in decoded_token):
+        return {"error": "invalid_token"}, 400
+    
+    # Get the user id from the token
+    user_id = decoded_token["user-id"]
+
+    # Get the request body
+    request_data = request.get_json()
+    email_address = request_data["email-address"].strip()
+
+    # If the email is invalid, return an error
+    try:
+        email_info = validate_email(email_address, check_deliverability=True)
+        email_address = email_info.normalized
+    except EmailNotValidError as e:
+        print(str(e))
+        print("Invalid email address")
+        return {"error": "invalid_email"}, 400
+
+    # If the email already exists, return an error
+    if (email_exists_in_database(email_address)):
+        print("Email exists")
+        return {"error": "email_exists"}, 400
+
+    # Update the email address
+    update_email_address(user_id, email_address)
+
+    print("LOL")
+    return {"success": True}, 200
+
+@app.route("/api/account/update-streak", methods=["POST"])
+def update_streak_api():
+    # Get the token from the request
+    token = request.cookies["token"]
+
+    # Decode the token
+    decoded_token = decode_jwt_token(token)
+
+    if ("error" in decoded_token):
+        return {"error": "invalid_token"}, 400
+    
+    # Get the user id from the token
+    user_id = decoded_token["user-id"]
+
+    # Update the user's streak
+    updated_streak = reset_streak_if_expired(user_id)
+
+    # Format the streak
+    formatted_streak = format_streak(updated_streak)
+
+    # Send the updated streak to the frontend
+    return {"success": True, "streak": formatted_streak}, 200
